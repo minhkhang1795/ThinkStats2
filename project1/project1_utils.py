2257a8b8e8783670ba3302462c682cbdd5f75a76
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-03-05 17:17:08 -0500

    Document utils file

--- project1/project1_utils.py
+++ project1/project1_utils.py
@@ -1,11 +1,6 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
-from thinkstats2 import Pmf, Cdf
-
 import thinkstats2
-import thinkplot
 
 """
 Function to group a DataFrame into bins
@@ -22,6 +17,8 @@ Return
 bins:       a list of bin names
 groups:     a GroupBy object
 """
+
+
 def GroupToBins(df, group_name='cmputr_time', bin_size=1):
     dataSeries = df[group_name]
     bins = np.arange(dataSeries.min(), dataSeries.max() + 1, bin_size)
@@ -43,6 +40,8 @@ Return
 bins:         a list of bin names
 groups:       a list of percentages corresponding to each group
 """
+
+
 def CalculatePercentages(groupResults, variable='depression_lvl'):
     bins, groups = groupResults
     perc = []  # Percentage list
@@ -51,6 +50,26 @@ def CalculatePercentages(groupResults, variable='depression_lvl'):
         positives = group[variable][group[variable] > 0].count()  # Having anxiety/depression
         perc.append(positives / total * 100)
     return np.array(bins), np.array(perc)
+
+
+"""
+Take a sample from a distribution and run many iterations
+to calculate estimates
+
+Arguments
+----------
+df:             DataFrame
+group_name:     the column we want its values to become bins. 
+                The values have to be discrete.
+variable:       the variable of interest that will be analyzed/processed in each binned group
+Operation:      Operation to calculate the statistic
+bin_size:       size of the bins
+iters:          number of iterations
+
+Return
+---------
+estimates:      a list of estimates list
+"""
 
 
 def SamplingDistributions(df, group_name='cmputr_time', variable='depression_lvl', Operation=CalculatePercentages,
@@ -63,10 +82,36 @@ def SamplingDistributions(df, group_name='cmputr_time', variable='depression_lvl
     return np.array(t).transpose()
 
 
+"""
+Compute 90% CI of the estimates
+
+Arguments
+----------
+estimates:      a list of estimates
+
+Return
+---------
+CI:             90% CI of the estimates
+"""
+
+
 def ComputeCI(estimates):
     cdf = thinkstats2.Cdf(estimates)
     ci = cdf.ConfidenceInterval(90)
     return ci
+
+
+"""
+Compute 90% CI of a list of estimate lists
+
+Arguments
+----------
+estimatesList:      a list of estimate lists
+
+Return
+---------
+CIs:             a list of 90% CI of the estimates
+"""
 
 
 def ComputeCIs(estimatesList):
@@ -76,9 +121,12 @@ def ComputeCIs(estimatesList):
         cis.append(ci)
     return np.array(cis).transpose()
 
+
 """
 Operation: Calculate Proportional Percentage
 """
+
+
 def CalculateProportionalPercentage(groupResults, variable='depression_lvl'):
     _, perc = CalculatePercentages(groupResults, variable=variable)
-    return perc, perc[1]/perc[0] # Proportional percentage of depression (High / Low) 
+    return perc, perc[1] / perc[0]  # Proportional percentage of depression (High / Low)
